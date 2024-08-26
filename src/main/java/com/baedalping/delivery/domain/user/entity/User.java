ad1be2e054c7cda0353bdc3b46181a3e298045bb
harper <35358294+yooyouny@users.noreply.github.com>
2024-08-26 20:06:21 +0900

    feat: 유저 도메인 엔티티 정의 - User, UserRole, UserAddress 정의 - User와 UserAddress는 1:N관계

---
+++ src/main/java/com/baedalping/delivery/domain/user/entity/User.java
@@ -0,0 +1,71 @@
+package com.baedalping.delivery.domain.user.entity;
+
+import com.baedalping.delivery.global.common.AuditField;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "P_USERS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at is NULL")
+public class User extends AuditField {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long userId;
+
+  @Column(name = "username", nullable = false)
+  private String username;
+
+  @Column(name = "password", nullable = false)
+  private String password;
+
+  @Column(unique = true, nullable = false)
+  @Email
+  private String email;
+
+  @Column(name = "role")
+  @Enumerated(EnumType.STRING)
+  private UserRole role = UserRole.CUSTOMER;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserAddress> addressList = new ArrayList<>();
+
+  @Column(name = "is_public", nullable = false)
+  private boolean isPublic = false;
+
+  @Builder
+  public User(String username, String password, String email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  public void updateUserRole(UserRole role) {
+    this.role = role;
+  }
+
+  public void isPublic(){
+    this.isPublic = true;
+  }
+
+  public void addAddress(UserAddress address) {
+    addressList.add(address);
+    address.setUser(this);
+  }
+}
---
+++ src/main/java/com/baedalping/delivery/domain/user/entity/UserAddress.java
@@ -0,0 +1,50 @@
+package com.baedalping.delivery.domain.user.entity;
+
+import com.baedalping.delivery.global.common.AuditField;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Table(name = "P_USER_ADDRESS")
+@Entity
+@NoArgsConstructor
+@SQLRestriction("deleted_at is NULL")
+public class UserAddress extends AuditField {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "address_id")
+  private UUID addressId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "address", nullable = false)
+  private String address;
+
+  @Column(name = "zipcode", nullable = false, length = 5)
+  private String zipcode;
+
+  @Column(name = "address_alias", nullable = true)
+  private String alias;
+
+  @Builder
+  private UserAddress(User user, String address, String zipcode, String alias) {
+    this.user = user;
+    this.address = address;
+    this.zipcode = zipcode;
+    this.alias = alias;
+  }
+  public void setUser(User user){
+    this.user = user;
+  }
+}
