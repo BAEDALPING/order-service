fef67297d14422a6d06d2089adf97e2fea374051
harper <35358294+yooyouny@users.noreply.github.com>
2024-08-28 09:01:35 +0900

    chore: audit 필드에 유저네임을 넣는 빈 등록 - 필터 적용시 추가구현

--- src/main/java/com/baedalping/delivery/global/config/JpaAuditConfig.java
+++ src/main/java/com/baedalping/delivery/global/config/JpaAuditConfig.java
@@ -1,8 +1,15 @@
 package com.baedalping.delivery.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
-public class JpaAuditConfig {}
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaAuditConfig {
+  @Bean
+  public AuditorAware<String> auditorAware(){
+    return new AuditorAwareImpl();
+  }
+}
