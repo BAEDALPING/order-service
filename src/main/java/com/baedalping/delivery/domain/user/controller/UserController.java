095082d033196a2079e324d7c505beadbcbd2925
harper <35358294+yooyouny@users.noreply.github.com>
2024-08-28 12:47:46 +0900

    feat: 유저 탈퇴 API 구현 - 삭제 시 delete_at에 현재 sys값 넣고 is_public은 false로 처리

--- src/main/java/com/baedalping/delivery/domain/user/controller/UserController.java
+++ src/main/java/com/baedalping/delivery/domain/user/controller/UserController.java
@@ -2,12 +2,15 @@ package com.baedalping.delivery.domain.user.controller;
 
 import com.baedalping.delivery.domain.user.dto.UserCreateRequestDto;
 import com.baedalping.delivery.domain.user.dto.UserCreateResponseDto;
+import com.baedalping.delivery.domain.user.dto.UserReadResponseDto;
 import com.baedalping.delivery.domain.user.dto.UserUpdateRequestDto;
 import com.baedalping.delivery.domain.user.dto.UserUpdateResponseDto;
 import com.baedalping.delivery.domain.user.service.UserService;
 import com.baedalping.delivery.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,5 +37,11 @@ public class UserController {
     return ApiResponse.ok(
         userService.updateUserInfo(
             userId, requestDto.username(), requestDto.password(), requestDto.email()));
+  }
+
+  @DeleteMapping("/{userId}")
+  public ApiResponse<UserReadResponseDto> delete(@PathVariable("userId") Long userId){
+    // TODO :: spring security 적용 이후 principle에 있는 userId를 가져올 예정
+    return ApiResponse.ok(userService.delete(userId));
   }
 }
