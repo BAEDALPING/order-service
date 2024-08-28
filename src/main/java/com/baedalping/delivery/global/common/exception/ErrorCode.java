2527ebdee316eb7aa47b2edbacc1454ee4a58a7a
harper <35358294+yooyouny@users.noreply.github.com>
2024-08-28 16:29:57 +0900

    fix: 병합 충돌로 인한 커스텀 예외 중복건 수정해서 반영

--- src/main/java/com/baedalping/delivery/global/common/exception/ErrorCode.java
+++ src/main/java/com/baedalping/delivery/global/common/exception/ErrorCode.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
   NOT_FOUND_USER(HttpStatus.NOT_FOUND, "가입된 유저가 아닙니다"),
-  NOT_FOUND_USER(HttpStatus.NOT_FOUND, "가입된 유저가 아닙니다"),
   DUPLICATED_USER(HttpStatus.CONFLICT, "이미 가입된 유저 이메일 입니다"),
 
   NOT_FOUND_STORE_CATEGORY(HttpStatus.NOT_FOUND, "가게 분류를 찾을 수 없습니다."),
