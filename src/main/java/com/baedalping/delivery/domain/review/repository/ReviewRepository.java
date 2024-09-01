3a5de79e5acb248142f0747ea50d20e0430b2869
SerenityZenDev <tjrqo2000@gmail.com>
2024-09-01 10:46:00 +0900

    feat: 리뷰 조회 (#50) - User 용 리뷰 조회(신고 제외) - Owner, Admin 용 리뷰 조회(신고 포함)

--- src/main/java/com/baedalping/delivery/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/baedalping/delivery/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.baedalping.delivery.domain.review.repository;
 
 import com.baedalping.delivery.domain.review.entity.Review;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
+    List<Review> findByStore_StoreIdAndIsReportedFalse(UUID storeId);
+
+    List<Review> findByStore_StoreId(UUID storeId);
 }
 
